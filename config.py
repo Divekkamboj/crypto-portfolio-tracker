23f7d0c8bbfc47314c827de5eaab87a904a8e7b1
Divekkamboj <divekkamboj791@gmail.com>
2026-07-19 16:12:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Local PostgreSQL ka URL
-DATABASE_URL = "postgresql://postgres:aware%2332@localhost:5432/crypto_db"
+# Ab hum SQLite use karenge jo Render par bina kisi error ke chalega
+DATABASE_URL = "sqlite:///./crypto_portfolio.db"
 
-engine = create_engine(DATABASE_URL)
+# SQLite ke liye 'check_same_thread': False zaroori hai
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
